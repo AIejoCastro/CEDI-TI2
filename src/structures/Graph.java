2a47c59f95c2b10c5f3f4eed993f5a2554a9c97d
AIejoCastro <alejocastro20@hotmail.com>
2023-05-30 21:12:51 -0500

    Changes

---
+++ src/structures/Graph.java
@@ -0,0 +1,15 @@
+package structures;
+
+import java.util.List;
+
+public interface Graph<T> {
+    void addVertex(T data);
+    void addEdge(T source, T destination, int weight);
+    void removeVertex(T data);
+    void removeEdge(T sourceData, T destData);
+    List<T> bfs(T startVertex);
+    List<T> dfs(T startVertex);
+    // Agregar métodos para los algoritmos de Dijkstra y Floyd Warshall
+    List<T> dijkstra(T startVertex, T endVertex);
+    int[][] floydWarshall();
+}
---
+++ test/AdjacencyGraphTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+import structures.AdjacencyGraph;
+
+class AdjacencyGraphTest {
+    private AdjacencyGraph<Integer> graph;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+}
